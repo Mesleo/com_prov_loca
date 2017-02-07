c8fa4d0e7ec825e28c8671f12f46f0ce185ecb12
JaviBP <javifocus88@hotmail.com>
2017-02-07 19:45:14 +0100

    Add files via upload

---
+++ comunidad_autonoma.sql
@@ -0,0 +1,26 @@
+CREATE TABLE comunidad_autonoma (
+  id INTEGER (4) NOT NULL,
+  com_autonoma varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO comunidad_autonoma (id, com_autonoma)
+VALUES
+	(1,'Andalucía'),
+	(2,'Aragón'),
+	(3,'Asturias, Principado de'),
+	(4,'Balears, Illes'),
+	(5,'Canarias'),
+	(6,'Cantabria'),
+	(7,'Castilla y León'),
+	(8,'Castilla - La Mancha'),
+	(9,'Catalunya'),
+	(10,'Comunitat Valenciana'),
+	(11,'Extremadura'),
+	(12,'Galicia'),
+	(13,'Madrid, Comunidad de'),
+	(14,'Murcia, Región de'),
+	(15,'Navarra, Comunidad Foral de'),
+	(16,'País Vasco'),
+	(17,'Rioja, La'),
+	(18,'Ceuta'),
+	(19,'Melilla');
